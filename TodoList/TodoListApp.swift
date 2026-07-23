584ff10e64a90b1dc94c040c4a69d7122c5c142a
Guofeng Luo <luoguofeng0401@gmail.com>
2026-07-24 06:48:05 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Guofeng Luo on 2026/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
